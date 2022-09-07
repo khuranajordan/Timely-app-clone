723306919b6e7c1594640e4fe49670d51230f47f
Vishal Gupta <83306636+vishalg1021997@users.noreply.github.com>
2022-09-08 04:01:58 +0530

    login page

---
+++ src/component/Task.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Task = () => {
+  return (
+    <div>
+        <div>
+            <h1>Task Planning</h1>
+            <button>Share Feedback</button>
+            <button>Start tour</button>
+            <button>New Task</button>
+        </div>
+    </div>
+  )
+}
+
+export default Task
