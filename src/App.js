ca7eb4e21fff02a6fb1522b05066a11ecafaf571
Rohan Khurana <rohankhurana20@gmail.com>
2022-09-08 14:38:12 +0530

    Homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Footer from "./Components/Footer/Footer1";
+import HomePage from "./Components/Homepage/Homepage";
+
+export default function App() {
+  return (
+    <div className="App">
+      <HomePage />
+      <br />
+      <br />
+      <Footer />
+    </div>
+  );
+}
