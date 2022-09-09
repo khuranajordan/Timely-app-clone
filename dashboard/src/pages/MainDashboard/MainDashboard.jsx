27fcf11fe76d1ec009552a909e202fbc66ac9d79
sobebarali <sobebar.ali17@gmail.com>
2022-09-10 01:06:48 +0530

    ok

--- dashboard/src/pages/MainDashboard/MainDashboard.jsx
+++ dashboard/src/pages/MainDashboard/MainDashboard.jsx
@@ -1,51 +1,87 @@
 import React from "react";
+import brain from "../../Assets/brain.png";
+import location from "../../Assets/navigation.png";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import style from "./MainDashboard.module.css";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import Box from "@mui/material/Box";
-import dayjs from "dayjs";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+
+
+const style1 = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 const MainDashboard = () => {
-  const [value, setValue] = React.useState(dayjs());
+  const [alignment, setAlignment] = React.useState("day");
+  const [alignment2, setAlignment2] = React.useState("timeline");
+  const [alignment3, setAlignment3] = React.useState("solo");
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  const handleChange2 = (event2, newAlignment2) => {
+    setAlignment2(newAlignment2);
+  };
+
+  const handleChange3 = (event3, newAlignment3) => {
+    setAlignment3(newAlignment3);
+  };
+
   return (
     <>
       <Sidebar />
-
       <div className={style.top}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "flex-start",
-            "& > *": {
-              m: 2,
-            },
-          }}
-        >
-          <ButtonGroup variant="outlined" aria-label="outlined button group">
-            <Button>Day</Button>
-            <Button>Week</Button>
-            <Button>Month</Button>
-          </ButtonGroup>
-        </Box>
-        <div className={style.datePicker}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DesktopDatePicker
-              value={value}
-              minDate={dayjs("2017-01-01")}
-              onChange={(newValue) => {
-                setValue(newValue);
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
+        <div className="left">
+          <ToggleButtonGroup
+            color="primary"
+            size="small"
+            value={alignment}
+            exclusive
+            onChange={handleChange}
+            aria-label="Platform"
+            style={{ margin: "20px", height: "30px" }}
+          >
+            <ToggleButton value="day">Day</ToggleButton>
+            <ToggleButton value="week">Week</ToggleButton>
+            <ToggleButton value="month">Month</ToggleButton>
+          </ToggleButtonGroup>
         </div>
-        <div></div>
+        <div className="right">
+          <ToggleButtonGroup
+            color="primary"
+            size="small"
+            value={alignment3}
+            exclusive
+            onChange={handleChange3}
+            aria-label="Platform"
+            style={{ height: "30px" }}
+          >
+            <ToggleButton value="solo">Solo</ToggleButton>
+            <ToggleButton value="company">Company</ToggleButton>
+          </ToggleButtonGroup>
+        </div>
       </div>
 
       <div className={style.main}>
@@ -58,31 +94,80 @@ const MainDashboard = () => {
             <p>0h</p>
           </div>
           <div className={style.leftBottom}>
-              <button>New Entry</button>
+            <button onClick={handleOpen}>New Entry</button>
+            <Modal
+              hideBackdrop
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="child-modal-title"
+              aria-describedby="child-modal-description"
+            >
+              <Box sx={{ ...style1, width: 200 }}>
+                <TextField
+                  id="standard-helperText"
+                  label="Helper text"
+                  defaultValue="Default Value"
+                  helperText="Some important text"
+                  variant="standard"
+                />
+                <p id="child-modal-description">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                </p>
+                <Button onClick={handleClose}>Submit</Button>
+              </Box>
+            </Modal>
           </div>
         </div>
         <div className={style.right}>
           <div className={style.rightTop}>
             <div>
               <h4>Automatic time tracking</h4>
-              <button>i</button>
+              <button className={style.rightTopBtn}>i</button>
             </div>
             <div>
-              <button>Timeline</button>
-              <button>List</button>
+              <ToggleButtonGroup
+                color="primary"
+                size="small"
+                value={alignment2}
+                exclusive
+                onChange={handleChange2}
+                aria-label="Platform"
+                style={{ height: "30px" }}
+              >
+                <ToggleButton value="timeline">Timeline</ToggleButton>
+                <ToggleButton value="list">List</ToggleButton>
+              </ToggleButtonGroup>
             </div>
           </div>
           <div className={style.rightBottom}>
-            <h1>See Everyting</h1>
+            <h1>See everyting</h1>
             <h1>you worked upon</h1>
             <p>Never forget what you worked on. Install Memory for Windows</p>
             <p>to get started with automatic time tracking. Learn more</p>
-            <div className={style.leftBottom}>
-              <button>New Entry</button>
-          </div>
-          <div className={style.leftBottom}>
-              <button>New Entry</button>
-          </div>
+            <div className={style.rightBottomMemory}>
+              <img
+                src={brain}
+                alt="brain"
+                style={{ height: "40px", width: "auto" }}
+              />
+              <div>
+                <p>Memory for Windows</p>
+                <p>Capture the time you spend on apps and browser</p>
+              </div>
+              <p>Download</p>
+            </div>
+            <div className={style.rightBottomTracker}>
+              <img
+                src={location}
+                alt="tracker"
+                style={{ height: "40px", width: "auto" }}
+              />
+              <div>
+                <p>Location Tracker</p>
+                <p>Essential for the on-the-go professional</p>
+              </div>
+              <p>Connect</p>
+            </div>
           </div>
         </div>
       </div>
